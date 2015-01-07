b1365c6835259589fe97f8c9a2f0248166c26b6a
claytonrie <jonfieldthe2nd@gmail.com>
2015-01-07 12:28:07 -0500

    Reworked the Class Constructor Generator
    Got rid of toSource function.
    Got rid of Function constructors.
    Should work. There may be a bug in it.

--- RPG-Puzzle_Game/public_html/class.js
+++ RPG-Puzzle_Game/public_html/class.js
@@ -1,29 +1,11 @@
 // A Class Constructor Generator
 //     by Clayton
-toSource = function(obj){
-    switch(typeof obj){
-        case "string": return '"'+obj+'"'; // For strings
-        case "number": case "boolean": case "function":
-            return obj.toString(); // For number, booleans, and functions
-        case "object": default:
-            if(obj.toString().search("/") === 0)
-                return obj.toString(); // For RegExp
-            var temp = "";
-            if(Array.isArray(obj)){
-                for(var i in obj)
-                    temp += ((i !== 0)? (", ") : ("")) + toSource(obj[i]);
-                return "[" + temp + "]"; // For Arrays
-            }
-            for(var i in obj) // For "Object Objects", custom classes, and other
-                temp += ((i !== Object.getOwnPropertyNames(obj)[0])? (', "') : ('"')) + i + '": ' + toSource(obj[i]);
-            return "({" + temp + "})";
-    }
-};
 Class = function(obj){
-    var tempFuncBody = "", args = [];
-    for(var i in obj){
-        tempFuncBody += 'this["' + i + '"] = ' + toSource(obj[i]) + '; ';
-        if(i === "init")
-            for(var j = 0; j < obj[i].length; j++ ) args.push("_" + j);
-    } return (new Function(args.join(','), tempFuncBody + 'this["init"](' + args + ")"));
+    return (function(){
+        this.tempObjectHold = this.tempObjectHold || obj;
+        for(var i in this.tempObjectHold)
+            if(this.tempObjectHold.hasOwnProperty(i))
+                this[i] = this.tempObjectHold[i];
+        (this.tempObjectHold.init || function(){}).apply(this, arguments);
+    });
 };
